5cfce7a954fae69e9a1f418911f774f7f5f2cfd5
chinmaytope56-max <chinmaytope56@gmail.com>
2026-09-21 22:28:53 -0400

    Update find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 
+query=$1
+subject=$2
+output=$3
+
+tblastn \
+  -query "$query" \
+  -subject "$subject" \
+  -outfmt "6 qseqid sseqid pident length qlen qstart qend sstart send evalue bitscore" |
+awk '$3 > 30 && ($4/$5) > 0.90' > "$output"
+
+wc -l < "$output"
